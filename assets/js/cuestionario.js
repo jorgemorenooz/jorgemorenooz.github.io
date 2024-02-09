b38b1c9df12b92494b12cc6bb7f78e7e31be46d5
jorgemorenooz <jorgemorenozores@gmail.com>
2024-02-09 20:27:01 +0100

    cambios

--- assets/js/cuestionario.js
+++ assets/js/cuestionario.js
@@ -1,99 +1,34 @@
-const questions = [
-    {
-        id: "1",
-        question: "¿Cobra alguna ayuda de manera habitual?",
-        answers: [
-            { text: "Si", value: "true"},
-            { text: "No", value: "false"},
-        ],
-        range: "1",
-        parentQuestion: null,
-    },
-    {
-        id: "2",
-        question: "Indique si recibe alguna de las ayudas siguiente:",
-        answers: [
-            { text: "Prestación por desempleo", value: "true"},
-            { text: "ERTE", value: "true"},
-            { text: "Ninguna de las anteriores", value: "false"},
-        ],
-        range: "2",
-        parentQuestion: "1",
-    },
-    {
-        id: "3",
-        question: "Indique su fecha de nacimiento:",
-        answers: [
-            { text: "Fecha", value: "true"},
-        ],
-        range: "1",
-        parentQuestion: null,
-    },
-    {
-        id: "4",
-        question: "¿Tiene usted hij@(s)?",
-        answers: [
-            { text: "Si", value: "true"},
-            { text: "No", value: "false"},
-        ],
-        range: "1",
-        parentQuestion: null,
-    },
-    {
-        id: "5",
-        question: "Número de hij@(s)",
-        answers: [
-            { text: "Numero", value: "true"},
-        ],
-        range: "2",
-        parentQuestion: "4",
-    },
-    {
-        id: "6",
-        question: "Estimación de días cotizados en toda la vida laboral",
-        answers: [
-            { text: "Si", value: "true"},
-        ],
-        range: "1",
-        parentQuestion: null,
-    },
-    {
-        id: "7",
-        question: "Salario bruto (EUR por mes)",
-        answers: [
-            { text: "No", value: "true"},
-        ],
-        range: "1",
-        parentQuestion: null,
-    },
-];
-
+let questions = [];
+let texto = "";
 let currentQuestionIndex = 0;
 let questionNumber = 0;
 let value = "false";
+let edad_years = 21;
 
 let questionElement = document.getElementById("question");
 let answerButtons = document.getElementById("answer-buttons");
 let nextButton = document.getElementById("next-btn");
 
-function startQuiz(){
+function startQuiz(texto){
     currentQuestionIndex = 0;
     nextButton.innerHTML = "Siguiente";
-    fetchQuestions()
-    showQuestion();
+    fetchQuestions(texto);
 }
 
-function fetchQuestions() {
-    fetch('/assets/db/questions.json')
-        .then(response => response.json())
-        .then(data => {
-            var qq = data; // Almacena las preguntas del archivo JSON en la variable
-            console.log(qq);
-        })
-        .catch(error => {
-            console.error('Error al cargar las preguntas:', error);
-        });
+
+function fetchQuestions(tema) {
+    fetch('./assets/data/questions.json')
+    .then(response => response.json())
+    .then(data => {
+        questions = data[tema];
+        showQuestion();
+    })
+    .catch(error => {
+        console.error('Error al cargar las preguntas:', error);
+        showResult(false);
+    });
 }
+
 
 function showQuestion(){
     resetState();
@@ -101,15 +36,48 @@ function showQuestion(){
     questionNumber++;
     questionElement.innerHTML = questionNumber + ". " + currentQuestion.question;
 
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn");
-        answerButtons.appendChild(button);
-        button.dataset.answerValue = answer.value;
-        button.addEventListener("click", selectAnswer)
-    });
+    if (currentQuestion.input === "button") {
+        // Crear botones como opciones de respuesta
+        currentQuestion.answers.forEach(answer => {
+            const button = document.createElement("button");
+            button.innerHTML = answer.text;
+            button.classList.add("btn");
+            answerButtons.appendChild(button);
+            button.dataset.answerValue = answer.value;
+            button.addEventListener("click", selectAnswer)
+        });
+    } else if (currentQuestion.input === "date") {
+        // Crear campo de entrada de fecha
+        const dateInput = document.createElement("input");
+        dateInput.setAttribute("type", "date");
+        dateInput.classList.add("date-input");
+        dateInput.addEventListener("change", () => {
+            const fechaNacimiento = new Date(dateInput.value);
+            const hoy = new Date();
+            const diffTime = Math.abs(hoy - fechaNacimiento);
+            const edad = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 365)); // Calcular la edad en años
+    
+            value = edad;
+            edad_years = edad;
+            console.log(value);
+            nextButton.style.display = "block";
+        });
+        answerButtons.appendChild(dateInput);
+
+    } else if (currentQuestion.input === "number") {
+        // Crear campo de entrada de número
+        const numberInput = document.createElement("input");
+        numberInput.setAttribute("type", "number");
+        numberInput.classList.add("number-input");
+        numberInput.setAttribute("min", "0");
+        numberInput.addEventListener("input", () => {
+            value = numberInput.value; // Asignar el valor del número como la respuesta seleccionada
+            nextButton.style.display = "block";
+        });
+        answerButtons.appendChild(numberInput);
+    }
 }
+
 
 function resetState(){
     nextButton.style.display = "none";
@@ -117,6 +85,7 @@ function resetState(){
         answerButtons.removeChild(answerButtons.firstChild)
     }
 }
+
 
 function selectAnswer(event){
     const selectedBtn = event.target;
@@ -132,48 +101,50 @@ function selectAnswer(event){
     }
 }
 
-function handleNextButton(){
+
+function handleNextButton() {
     const currentQuestion = questions[currentQuestionIndex];
+    const condition = currentQuestion.condition; // Obtener la condición de la pregunta actual
 
-    currentQuestionIndex++;
-    if(currentQuestionIndex < questions.length){
-        var currentRange = parseInt(currentQuestion.range);
+    if (condition === null || (condition !== null && value !== null && eval(condition))) {
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length) {
+            var currentRange = parseInt(currentQuestion.range);
 
-        if (value === "true") {
-            const nextQuestionWithParent = questions.find(question =>
-                question.parentQuestion === currentQuestion.id
-            );
-
-            if (nextQuestionWithParent && nextQuestionWithParent.range == (currentRange+1).toString()) {
-                currentQuestionIndex = questions.indexOf(nextQuestionWithParent);
-            }
-        }else {
-            currentRange = 1;
-            let nextQuestionIndex = currentQuestionIndex;
-            while (nextQuestionIndex < questions.length) {
-                const nextQuestion = questions[nextQuestionIndex];
-                const nextRange = parseInt(nextQuestion.range);
-
-                if (nextRange === currentRange) {
-                    currentQuestionIndex = nextQuestionIndex;
-                    break;
-                }
-                nextQuestionIndex++;
-            }
+            //LOgica....
+            //....
+            //....
+            
+            showQuestion();
+        } else {
+            showResult(true);
         }
-        showQuestion();
-    }
-    else{
-        showResult();
+    } else {
+        showResult(false);
     }
 }
 
-function showResult(){
+
+function showResult(valid){
     var result = 100;
     resetState();
-    questionElement.innerHTML = `Según nuestros cálculos, puedes ser beneficiario de ${result}€`;
+
+    if (valid == true){
+        questionElement.innerHTML = `Según nuestros cálculos, puedes ser beneficiario de ${result}€`;
+    }
+    else{
+        currentQuestionIndex = questions.length+1;
+        questionElement.innerHTML = `No Apto para la subvención`;
+    }
+    
     nextButton.innerHTML = "Volver a responder";
     nextButton.style.display = "block";
+}
+
+function mostrarCuestionario(){
+    var linkElement = event.target;
+    texto = linkElement.innerText;
+    startQuiz(texto);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -189,9 +160,7 @@ document.addEventListener("DOMContentLoaded", function() {
             handleNextButton();
         }else{
             questionNumber = 0;
-            startQuiz();
+            startQuiz(texto);
         }
     });
-
-    startQuiz();
 });
